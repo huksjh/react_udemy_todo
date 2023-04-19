11c25234c2a61acc013ca941225ba90c0806f0e3
huks <wkdgur1209@gmail.com>
2023-04-19 16:50:31 +0900

    동적 css 추가 샘플

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,29 +1,40 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import Button from "../../UI/Button/Button";
+import "./CourseInput.css";
 
-const CourseInput = props => {
-  const [enteredValue, setEnteredValue] = useState('');
+const CourseInput = (props) => {
+    const [enteredValue, setEnteredValue] = useState("");
+    // 필수 항목 체크 - 기본 설정 true
+    const [isValid, setIsValid] = useState(true);
 
-  const goalInputChangeHandler = event => {
-    setEnteredValue(event.target.value);
-  };
+    const goalInputChangeHandler = (event) => {
+        // 값이 있으면 경고 초기화
+        if (event.target.value.trim().length > 0) {
+            setIsValid(true);
+        }
+        setEnteredValue(event.target.value);
+    };
 
-  const formSubmitHandler = event => {
-    event.preventDefault();
-    props.onAddGoal(enteredValue);
-  };
+    const formSubmitHandler = (event) => {
+        event.preventDefault();
+        // 값이 없으면 입력안되게
+        if (enteredValue.trim().length === 0) {
+            setIsValid(false);
+            return;
+        }
+        props.onAddGoal(enteredValue);
+    };
 
-  return (
-    <form onSubmit={formSubmitHandler}>
-      <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
-      </div>
-      <Button type="submit">Add Goal</Button>
-    </form>
-  );
+    return (
+        <form onSubmit={formSubmitHandler}>
+            <div className={`form-control ${!isValid ? "invalid" : ""}`}>
+                <label>Course Goal</label>
+                <input type="text" onChange={goalInputChangeHandler} />
+            </div>
+            <Button type="submit">Add Goal</Button>
+        </form>
+    );
 };
 
 export default CourseInput;
